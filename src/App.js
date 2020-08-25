ee00734dbfb5baad22f20385747ea6b53e00cdb3
charieScript <gozione3@gmail.com>
2020-08-25 22:39:27 +0100

    added action types and action creators

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CakeContainer from './components/CakeContainer';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <CakeContainer />
     </div>
   );
 }
